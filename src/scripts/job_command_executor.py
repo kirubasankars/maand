5d8521d185c4a6a3a7ab5e2b2c187cd3eb304aed
Kiruba Sankar Swaminathan <kiruba.sankars@gmail.com>
2024-11-04 09:12:11 -0500

    job_command_executor

--- src/scripts/job_command_executor.py
+++ src/scripts/job_command_executor.py
@@ -11,22 +11,12 @@ import utils
 logger = utils.get_logger()
 
 def execute_command(job, command):
-
     if "/commands" not in sys.path:
         sys.path.append("/commands")
     os.makedirs("/commands", exist_ok=True)
 
     with maand_job.get_db() as db:
         cursor = db.cursor()
-
-        cursor.execute("SELECT job_name, name as command_name, depend_on_config as config FROM job_commands WHERE depend_on_job = ? AND depend_on_command = ? ", (job, command, ))
-        rows = cursor.fetchall()
-        context = []
-        for job_name, command_name, config in rows:
-            context.append({"job": job_name, "command": command_name, "config": json.loads(config) })
-
-        with open(f"/commands/{job}/context.json", "w") as f:
-            f.write(json.dumps(context))
 
         cursor.execute("SELECT path, content, isdir FROM job_files WHERE job_id = (SELECT job_id FROM job WHERE name = ?) AND path like ? ORDER BY isdir DESC", (job, f"{job}/_modules%"))
         rows = cursor.fetchall()
@@ -37,14 +27,27 @@ def execute_command(job, command):
                 continue
             with open(f"/commands/{path}", "wb") as f:
                 f.write(content)
+
+        cursor.execute("SELECT job_name, name as command_name, depend_on_config as config FROM job_commands WHERE depend_on_job = ? AND depend_on_command = ?", (job, command, ))
+        rows = cursor.fetchall()
+
+        context = []
+        for job_name, command_name, config in rows:
+            context.append({"job": job_name, "command": command_name, "config": json.loads(config) })
+
+        with open(f"/commands/{job}/_modules/context.json", "w") as f:
+            f.write(json.dumps(context))
     try:
         job_command = importlib.import_module(f'{job}._modules.command_{command}')
         if "execute" in dir(job_command):
+            os.chdir(f"/commands/{job}/_modules")
             job_command.execute()
+            os.chdir(f"/")
         else:
             logger.error(f"execute method not found: command_{command}")
     except ModuleNotFoundError:
         logger.error(f"command not found: job = {job}, command = command_{command}")
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
